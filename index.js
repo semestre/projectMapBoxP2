fc65ca285a7c4c185f6b0f580464ed2a51ac0c12
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-05-05 19:21:24 -0600

    fixed the thing

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-
 const app = express();
 const PORT = 3000;
+
+// Serve CSS, JS, images, folders, etc.
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "map.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/map.html`);
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
