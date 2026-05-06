67708254759176622426c17ad74ff7cabbed84d4
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-05-05 18:32:54 -0600

    made the link to map.html

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/map.html`);
 });
